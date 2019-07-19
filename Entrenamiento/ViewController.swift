99ecaf0d79a8a7f1d456ef9a53266c7dcaf73f0d
jesussarco <sarco.jesus@gmail.com>
2019-07-19 20:55:08 +0200

    Initial Commit

---
+++ Entrenamiento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Entrenamiento
+//
+//  Created by Jesus Alberto Sarco Giannini on 19/07/2019.
+//  Copyright © 2019 Smart Builders C.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
