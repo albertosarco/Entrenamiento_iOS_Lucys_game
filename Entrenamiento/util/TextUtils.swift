0366d3f52d7e7c6649f69bd4955f8181097e4ce9
jesussarco <sarco.jesus@gmail.com>
2019-07-22 17:08:55 +0200

    initial commit

---
+++ Entrenamiento/util/TextUtils.swift
@@ -0,0 +1,14 @@
+//
+//  TextUtils.swift
+//  Entrenamiento
+//
+//  Created by Jesus Alberto Sarco Giannini on 21/07/2019.
+//  Copyright © 2019 Smart Builders C.A. All rights reserved.
+//
+
+class TextUtils {
+    
+    static func isEmpty(_ s: String!) -> Bool {
+        return s == nil || s.count == 0
+    }
+}
